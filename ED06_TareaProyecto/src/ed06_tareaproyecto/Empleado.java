659165a179f298bfd28d7bf1c656e9d58497335b
Casa-PC <Casa-PC@JUANKA>
2017-05-23 15:41:35 +0200

    Nuevo repositorio

---
+++ ED06_TareaProyecto/src/ed06_tareaproyecto/Empleado.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed06_tareaproyecto;
+
+/**
+ *
+ * @author ED06
+ */
+public class Empleado {
+    private String dni;
+    private String nombre;
+    private String direccion;
+    private String cargo;
+    private int numHijos;
+    
+    public Empleado (String dni, String nombre){
+        this.dni=dni;
+        this.nombre=nombre;
+        this.cargo="EMPLEADO";
+        this.numHijos=0;
+    }
+    
+    /**
+     *
+     * @param base the value of base
+     * @return the int
+     */
+    public int calcula_sueldo(int base){
+        int total=base;
+        
+        if(cargo.equals("ENCARGADO"))
+            total+=300;
+        else if(cargo.equals("DIRECTOR"))
+            total+=1000;
+        if(numHijos>=3)
+            total+=numHijos*50;
+        return total;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the cargo
+     */
+    public String getCargo() {
+        return cargo;
+    }
+
+    /**
+     * @return the numHijos
+     */
+    public int getNumHijos() {
+        return numHijos;
+    }
+
+    /**
+     * @param numHijos the numHijos to set
+     */
+    public void setNumHijos(int numHijos) {
+        this.numHijos = numHijos;
+    }
+    
+    
+    
+}
--- ED06_TareaProyecto/src/ed06_tareaproyecto/Main.java
+++ ED06_TareaProyecto/src/ed06_tareaproyecto/Main.java
@@ -1,8 +1,4 @@
 
- //* To change this license header, choose License Headers in Project Properties.
- //* To change this template file, choose Tools | Templates
- //* and open the template in the editor.
-
 package ed06_tareaproyecto;
 
 /**
@@ -15,19 +11,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado("1245678M","Perico de los Palotes");
         
-        imprimir_emp(empleado1);
+        Empleado Empleado1 = new Empleado("1245678M","Perico de los Palotes");
+        
+        imprimir_emp(Empleado1);
     }
 
-    public static void imprimir_emp(Empleado empleado1) {
-        empleado1.setDireccion("C/del Pez,6");
-        empleado1.setNumHijos(3);
-        System.out.println("DNI: "+empleado1.getDni());
-        System.out.println("Nombre: "+empleado1.getNombre());
-        System.out.println("Cargo: "+empleado1.getCargo());
-        System.out.println("Número de hijos: "+empleado1.getNumHijos());
-        System.out.println("Sueldo: "+empleado1.calcula_sueldo(1200));
+    public static void imprimir_emp(Empleado Empleado1) {
+        Empleado1.setDireccion("C/del Pez,6");
+        Empleado1.setNumHijos(3);
+        System.out.println("DNI: "+Empleado1.getDni());
+        System.out.println("Nombre: "+Empleado1.getNombre());
+        System.out.println("Cargo: "+Empleado1.getCargo());
+        System.out.println("Número de hijos: "+Empleado1.getNumHijos());
+        System.out.println("Sueldo: "+Empleado1.calcula_sueldo(1200));
     }
     
 }
